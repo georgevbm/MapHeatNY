fd846c93a4a2e4193a72966ecdf7834649c383e2
George Victor <george.vbm@hotmail.com>
2018-12-08 20:18:17 -0200

    Comunicação fron-end e back-end funcionando.

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const Venue = require('./models/Venue');
+const fs = require('fs');
+
+venuesDB = 206415;
+i = 0;
+venuesArray = new Array();
+
+// createJSON();
 
 // Config
 // Template Engine
@@ -14,12 +21,42 @@ app.use(bodyParser.json());
 
 // Rotas
 app.get("/", function (req, res) {
-    Venue.all().then(function (response) {
-        res.render('mapa', { venues: response });
-    });
+    // Venue.all().then(function (response) {
+    res.render('mapa');
+    // });
+});
+
+app.get("/venues", function (req, res) {
+    res.sendFile(__dirname+"/venue.json");
 });
 
 app.listen(3000, function () {
     console.log("Servidor rodando...");
 });
 
+function createJSON() {
+    Venue.findAll().then(function (response) {
+        while (i < venuesDB) {
+            var dict = {
+                venue: {
+                    name: response[i].get('name'),
+                    lat: response[i].get('lat'),
+                    lon: response[i].get('lon'),
+                    checkedUser: response[i].get('checkeduser')
+                },
+            };
+            venuesArray.push(dict);
+            i++;
+        }
+
+        var venuesJSON = JSON.stringify(venuesArray);
+
+        fs.writeFile('venue.json', venuesJSON, 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+        });
+    });
+}
